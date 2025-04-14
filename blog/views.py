7f42a976a1e80f7730164d6c2ef5ee506395a706
sauron <kravshanov97@gmail.com>
2025-04-14 13:26:48 +0500

    added a few changes

--- blog/views.py
+++ blog/views.py
@@ -108,11 +108,11 @@ class PostDetailApiView(APIView):
         try:
             post = Post.objects.get(pk=pk)
         except Post.DoesNotExist:
-            return Response({"message": "Post Not Found.!"})
+            return Response({"message": "Post Not Found.!"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = PostSerializer(post)
 
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 # get your posts
